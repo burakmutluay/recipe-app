c26d4720930701ad4118ae1e2b663724ab35bf20
burakmutluay <mburakmutluay@gmail.com>
2020-05-23 02:54:11 +0300

    added UnitOfMeasure

--- src/main/java/com/burak/recipe/domain/Recipe.java
+++ src/main/java/com/burak/recipe/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.burak.recipe.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -17,11 +18,16 @@ public class Recipe {
     private String url;
     private String directions;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
     @Lob
     private Byte[] image;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+
 
 
     public Long getId() {
@@ -102,5 +108,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
